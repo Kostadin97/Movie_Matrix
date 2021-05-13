ce46ca23f87f476bafca472eb38420a3fa3ca97e
Kostadin Seymenski <seimenski@icloud.com>
2021-05-13 15:57:36 +0300

    Comment schema created

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+
+const mongoURI =
+  "mongodb+srv://admin:admin@cluster.6ztzo.mongodb.net/themoviedb?retryWrites=true&w=majority";
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(`MongoDB Connected`);
+  });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server Running at ${PORT}`));
